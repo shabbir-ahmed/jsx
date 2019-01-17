d40b1eef3fff5badb13a2db59719a01fd2cfa2c8
shabbir3s <shabbir@salahsoftwaresolution.com>
2019-01-17 10:36:40 +0600

    Update jsx file

--- src/index.js
+++ src/index.js
@@ -4,28 +4,50 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 
-class App extends React.Component {
+// class App extends React.Component {
 
-    render(){
-        const headingTag = this.props.head;
-        return (
-            <div>
-                <h1>{headingTag}</h1>
-                <p>{this.props.sub}</p>
-            </div>
-        );
+//     render(){
+//         const headingTag = this.props.head;
+//         return (
+//             <div>
+//                 <h1>{headingTag}</h1>
+//                 <p>{this.props.sub}</p>
+//             </div>
+//         );
+//     }
+// }
+
+const arrayData = [
+    {
+        id: "bfjkdfkj",
+        name: "Shabbir Ahmed",
+        city: "Sylhet",
+        profession: "Web Dev"
+    },
+    {
+        id: "bfjkdfkj",
+        name: "Shabbir Ahmed",
+        city: "Sylhet",
+        profession: "Web Dev"
+    },
+    {
+        id: "bfjkdfkj",
+        name: "Shabbir Ahmed",
+        city: "Sylhet",
+        profession: "Web Dev"
     }
-}
+];
 
-// const App = (props) => {
-//     const headingTag = props.head;
-//     return (
-//         <div>
-//             <h1>{headingTag}</h1>
-//             <p>{props.sub}</p>
-//         </div>
-//     );
-// };
+const App = (props) => {
+    const headingTag = props.head;
+    return (
+        <div>
+            <h1>{headingTag}</h1>
+            <p>{props.sub}</p>
+            {console.log(arrayData)}
+        </div>
+    );
+};
 
 // Take the react component and it on the screen
 
